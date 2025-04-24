3d0dcce4cba222aec9f0dfe1e9f6359c567de404
Crspy <crspy8687@gmail.com>
2025-04-24 15:24:08 -0700

    Added info section to main page
    Added a section to main page containing screenshot of number table and some informative segments

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Hero } from "@/components/hero.tsx"
 import { Services } from "@/components/services.tsx"
+import { Info } from "@/components/info.tsx"
 import { Timeline } from "@/components/ui/timeline.tsx"
 import { ReviewCards } from "@/components/reviews.tsx"
 import { FAQ } from "@/components/FAQ.tsx"
@@ -10,17 +11,18 @@ export const Route = createFileRoute('/')({
 })
 
 function HomeComponent() {
-    return (
-        <main>
-            <div className="min-h-screen flex flex-col">
-                <div className="flex-grow">
-                    <Hero />
-                    <Services />
-                </div>
-            </div>
-            <Timeline />
-            <ReviewCards />
-            <FAQ />
-        </main>
-    )
+  return (
+    <main>
+      <div className="min-h-screen flex flex-col">
+        <div className="flex-grow">
+          <Hero />
+          <Services />
+        </div>
+      </div>
+      <Info />
+      <Timeline />
+      <ReviewCards />
+      <FAQ />
+    </main>
+  )
 }
